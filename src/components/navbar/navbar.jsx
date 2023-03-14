96a444ee21f01750726388e3b7d74c735512b6b1
sean-code <john.nganga@student.moringaschool.com>
2023-03-14 12:54:53 +0300

    Add extra components

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -16,10 +16,9 @@ function NavBar() {
                 <ul class="menu-items">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
-                    <li><a href="#food">Category</a></li>
-                    <li><a href="#food-menu">Menu</a></li>
-                    <li><a href="#testimonials">Testimonial</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#food-menu">Testimonials</a></li>
+                    <li><a href="#testimonials">Contact</a></li>
                 </ul>
                 <h1 class="logo">Hair Mastery ✂️ </h1>
             </div>
