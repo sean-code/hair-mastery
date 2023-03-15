1669bb3f552e1a68336f6a6a67603509ae5cb349
sean-code <john.nganga@student.moringaschool.com>
2023-03-15 13:08:14 +0300

    Add popular styles

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -2,6 +2,7 @@ import Slider from './carousel';
 import './landing.css';
 import { Image } from './image';
 import { Demo } from './video';
+import Popular from './popular';
 
 
 const Home = () => {
@@ -10,8 +11,8 @@ const Home = () => {
             <Slider />
                 <h2 style={{marginTop:'50px', fontWeight:'700'}}>Book an Appointment to Any of Our Outlets</h2>
             <Image />
-          
-
+                <h2>Our Popular Treats</h2>
+                <Popular />
             <Demo />
         </>
     )
