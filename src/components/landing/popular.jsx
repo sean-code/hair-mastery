78bafc92ce6c2a1cf69a9278cc9720133ac70f1f
sean-code <john.nganga@student.moringaschool.com>
2023-03-15 13:42:07 +0300

    Render popular treats

---
+++ src/components/landing/popular.jsx
@@ -0,0 +1,18 @@
+import serviceData from "../../assets/data/serviceData";
+import ServiceItem from "../UI/serviceItem";
+import { Container, Row } from "react-bootstrap";
+import './landing.css';
+
+const Popular = () => {
+    return (
+        <Container fluid>
+            <Row className="justify-content-center">
+                {serviceData.slice(0, 6).map((item) => (
+                    <ServiceItem item={item} key={item.id} className="mx-3 mb-4" />
+                ))}
+            </Row>
+        </Container>
+    );
+};
+
+export default Popular;
