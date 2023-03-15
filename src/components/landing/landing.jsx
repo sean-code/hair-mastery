2951afb7402fb167f296d53940bddfe5e624e541
sean-code <john.nganga@student.moringaschool.com>
2023-03-15 09:48:58 +0300

    Add service data to render our popular services

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return(
         <>
             <Slider />
-                <h2 style={{marginTop:'50px', fontWeight:'700'}}>Book An Appointment To Any Of Our Outlets</h2>
+                <h2 style={{marginTop:'50px', fontWeight:'700'}}>Book an Appointment to Any of Our Outlets</h2>
             <Image />
 
             <Demo />
