b5e251c1842097f2ccd060f647f86a83bb736d59
sean-code <john.nganga@student.moringaschool.com>
2023-03-16 11:03:59 +0300

    Update components

--- src/components/landing/popular.jsx
+++ src/components/landing/popular.jsx
@@ -6,7 +6,7 @@ import './landing.css';
 const Popular = () => {
     return (
         <Container fluid>
-            <Row className="justify-content-center">
+            <Row className="justify-content-center" style={{minWidth:'20px'}}>
                 {serviceData.slice(0, 6).map((item) => (
                     <ServiceItem item={item} key={item.id} className="mx-3 mb-4" />
                 ))}
