1da0565a0245d201fea00a1dea34a4d0dc4472be
sean-code <john.nganga@student.moringaschool.com>
2023-03-16 12:02:06 +0300

    Separate Link functionality from button functionality

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,21 +1,26 @@
 import serviceData from "../../assets/data/serviceData";
 import { Container, Row } from "react-bootstrap";
 import ServiceItem from "../UI/serviceItem";
+import { useState } from "react";
 
 const Services = () =>{
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (item) =>{
+    console.log(item)
+  };
+
     return(
         <>
             <Container>
                 <Row>
                     <h1>Services</h1>
-            {serviceData.map((item) => (
-              <ServiceItem item={item} key={item.id} />
+                {serviceData.map((item) => (
+              <ServiceItem item={item} key={item.id} handleClick={handleClick} />
             ))}
+            
           </Row>
             </Container>
-
-            
-
         </>
     )
 }
